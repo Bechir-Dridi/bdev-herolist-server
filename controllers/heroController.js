94824b080ded6b59dab16d84525a8776b7a0280e
Bechir-Dridi <bechir7dridi#gmail.com>
2024-03-18 23:35:23 +0100

    "serviceAccountKey.json" added to .env

--- controllers/heroController.js
+++ controllers/heroController.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const { colRef } = require("../firebase")
 //firebase:
 const { db } = require("../firebase")
@@ -18,7 +19,19 @@ const { storage } = require("../firebase")
 var admin = require("firebase-admin")
 
 //var serviceAccount = require("../serviceAccountKey.json");
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+const serviceAccount = {
+    "type": process.env.SERVICE_ACCOUNT_TYPE,
+    "project_id": process.env.SERVICE_ACCOUNT_PROJECT_ID,
+    "private_key_id": process.env.SERVICE_ACCOUNT_PRIVATE_KEY_ID,
+    "private_key": process.env.SERVICE_ACCOUNT_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "client_email": process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
+    "client_id": process.env.SERVICE_ACCOUNT_CLIENT_ID,
+    "auth_uri": process.env.SERVICE_ACCOUNT_AUTH_URI,
+    "token_uri": process.env.SERVICE_ACCOUNT_TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL,
+    "client_x509_cert_url": process.env.SERVICE_ACCOUNT_CLIENT_X509_CERT_URL,
+    "universe_domain": process.env.SERVICE_ACCOUNT_UNIVERSE_DOMAIN
+};
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -442,7 +455,7 @@ const like = async (req, res) => {
     try {
         const DocRef = doc(db, "heroes", id)
         const snapshotHero = await getHero(id);
-        console.log("SNAP SNAP", snapshotHero);
+        //console.log("SNAP SNAP", snapshotHero);
 
 
         //let DISLIKE = null;
@@ -525,7 +538,7 @@ const dislike = async (req, res) => {
         console.log("SNAP SNAP", snapshotHero);
 
 
-        let DISLIKE = null;
+        // let DISLIKE = null;
 
         let EMAIL = email;
         //if (dislike) { DISLIKE = true; }
@@ -602,10 +615,10 @@ const isHeroFunc = async (req, res) => {
     try {
         const DocRef = doc(db, "heroes", id)
         const snapshotHero = await getHero(id);
-        console.log("SNAP SNAP", snapshotHero);
+        //console.log("SNAP SNAP", snapshotHero);
 
 
-        let isHero = null;
+        //let isHero = null;
 
         let EMAIL = email;
         //if (dislike) { DISLIKE = true; }
@@ -682,10 +695,10 @@ const isVillainFunc = async (req, res) => {
     try {
         const DocRef = doc(db, "heroes", id)
         const snapshotHero = await getHero(id);
-        console.log("SNAP SNAP", snapshotHero);
+        //console.log("SNAP SNAP", snapshotHero);
 
 
-        let isVillain = null;
+        //let isVillain = null;
 
         let EMAIL = email;
         //if (dislike) { DISLIKE = true; }
